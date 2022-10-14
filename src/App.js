85650964d4add981f77fa275fff1d35dbb1178bd
diathekez <96537263+diathekez@users.noreply.github.com>
2022-10-14 15:19:04 -0500

    Landing page working. API consuming correctly.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import React, { Component } from 'react';
 import './App.css';
 import Header from './Header'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios';
 import { render } from '@testing-library/react';
 
+
+// This fetches the API and grabs the info from the array
 function App() {
   const [quote, setQuote] = useState('')
   const getQuote = () => {
@@ -18,13 +18,86 @@ function App() {
     })
   }
 
+
+// Generates random quote upon page loading 
+  useEffect(() => {getQuote()}, []);
+
   return (
     <div className="App">
-      <header classname="">
-        
-      </header>
-      <button onClick={getQuote}>Random Quote</button>
-      {quote && <h1>{quote}</h1>}
+        <section className="hero is-dark is-fullheight">
+          <div className="hero-head">
+            <header className="navbar" id="navbar-color">
+              <div className="container">
+                <div className="navbar-brand">
+                  <a className="navbar-item">
+                    Quoth the Raven
+                  </a>
+                  <span className="navbar-burger" data-target="navbarMenuHeroC">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                  </span>
+                </div>
+                <div id="navbarMenuHeroC" className="navbar-menu">
+                  <div className="navbar-end">
+                    <a className="navbar-item is-active">
+                      Home
+                    </a>
+                    <a className="navbar-item">
+                      API
+                    </a>
+                    <a className="navbar-item">
+                      About
+                    </a>
+                    <span className="navbar-item">
+                      <a className="button is-success is-inverted">
+                        <span className="icon">
+                          <i className="fab fa-github"></i>
+                        </span>
+                        <span>Github Repo</span>
+                      </a>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </header>
+          </div>
+
+          <div className="hero-body">
+            <div className="container has-text-centered">
+            {/* The random quote shows up here */}
+            <h1 className="card card-content is-centered title small-box">{quote}</h1>
+            {/* This button generates a random quote */}
+            <button className="button is-danger off-white" onClick={getQuote}>Generate Quote</button>
+            </div>
+          </div>
+
+          <div className="hero-body">
+            <div className="container has-text-centered">
+              <p className="title">
+                Please take five minutes of your time to think.
+              </p>
+              <br></br>
+              <p className="subtitle">
+                Is what I'm thinking right now true?
+              </p>
+              <p className="subtitle">
+                Is it helpful?
+              </p>
+              <p className="subtitle">
+                Is it inspiring?
+              </p>
+              <p className="subtitle">
+                Is it necessary?
+              </p>
+              <p className="subtitle">
+                Is it kind?
+              </p>
+            </div>
+          </div>
+
+        </section>
+
     </div>
     
   );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-<script src="https://cdn.tailwindcss.com"></script>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
